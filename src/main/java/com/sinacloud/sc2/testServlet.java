5f5076ade991acfca7f6f17ece0680d4d35915a6
zhangyun5 <zhangyun5@staff.sina.com.cn>
2016-01-25 10:55:15 +0800

    容器的java示例

---
+++ src/main/java/com/sinacloud/sc2/testServlet.java
@@ -0,0 +1,43 @@
+package com.sinacloud.sc2;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class testMysql
+ */
+@WebServlet(name="testServlet",urlPatterns="/testServlet")
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public testServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
